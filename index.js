357372859c2f0821c87329d70406f422b43668c7
Florian PASTEUR <florian.c.p@pm.me>
2022-04-28 11:01:40 +0200

    Increase screenshot resolution by factor 2

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const Jimp = require('jimp');
 
-async function appWindow(screenshotFile, destinationFile, mockupFile) {
+async function appWindow(screenshotFile, destinationFile, mockupFile, zoom) {
   const screenshot = await Jimp.read(screenshotFile);
   const mask = await Jimp.read('assets/window-content-mask-full.png');
   const browserMockup = await Jimp.read(mockupFile);
-  const browserPlaceholder = browserMockup.clone();
 
-  screenshot.resize(1280, 708);
-  browserPlaceholder.opacity(0).composite(screenshot, 20, 73)
+  screenshot.resize(1280 * zoom * 2, 708 * zoom * 2);
+  mask.scale(zoom);
+  browserMockup.scale(zoom);
+
+  const browserPlaceholder = browserMockup.clone();
+  browserPlaceholder.opacity(0).composite(screenshot, 20 * zoom * 2, 73 * zoom * 2)
 
   browserPlaceholder.mask(mask);
   browserMockup.composite(browserPlaceholder, 0, 0).write(destinationFile);
@@ -16,10 +19,10 @@ async function appWindow(screenshotFile, destinationFile, mockupFile) {
 
 module.exports = {
   browser: async function (screenshot, destinationFile) {
-    return appWindow(screenshot, destinationFile, 'assets/browser-overlay.png');
+    return appWindow(screenshot, destinationFile, 'assets/browser-overlay.png', 1);
   },
   application: async function (screenshot, destinationFile) {
-    return appWindow(screenshot, destinationFile, 'assets/application-overlay.png');
+    return appWindow(screenshot, destinationFile, 'assets/application-overlay.png', 1);
   },
   monitor: async function (screenshot, destinationFile) {
     throw new Error("Unsupported operation")
