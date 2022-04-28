918123cddd1f94bdb17b5d3c2f2f101be58f60d7
Florian PASTEUR <florian.c.p@pm.me>
2022-04-28 13:35:48 +0200

    Implement monitor mockup

--- index.js
+++ index.js
@@ -3,17 +3,33 @@ const Jimp = require('jimp');
 async function appWindow(screenshotFile, destinationFile, mockupFile, zoom) {
   const screenshot = await Jimp.read(screenshotFile);
   const mask = await Jimp.read(`${__dirname}/assets/window-content-mask-full.png`);
-  const browserMockup = await Jimp.read(mockupFile);
+  const overlay = await Jimp.read(mockupFile);
 
   screenshot.resize(1280 * zoom * 2, 708 * zoom * 2);
   mask.scale(zoom);
-  browserMockup.scale(zoom);
+  overlay.scale(zoom);
 
-  const browserPlaceholder = browserMockup.clone();
-  browserPlaceholder.opacity(0).composite(screenshot, 20 * zoom * 2, 73 * zoom * 2)
+  const overlayCopy = overlay.clone();
+  overlayCopy.opacity(0).composite(screenshot, 20 * zoom * 2, 73 * zoom * 2)
 
-  browserPlaceholder.mask(mask);
-  browserMockup.composite(browserPlaceholder, 0, 0).write(destinationFile);
+  overlayCopy.mask(mask);
+  overlay.composite(overlayCopy, 0, 0).write(destinationFile);
+}
+
+async function monitor(screenshotFile, destinationFile, zoom) {
+  const screenshot = await Jimp.read(screenshotFile);
+  const mask = await Jimp.read(`${__dirname}/assets/monitor-mask.png`);
+  const overlay = await Jimp.read(`${__dirname}/assets/monitor-overlay.png`);
+
+  screenshot.resize(2461 * zoom, 1390 * zoom);
+  mask.scale(zoom);
+  overlay.scale(zoom);
+
+  const overlayCopy = overlay.clone();
+  overlayCopy.opacity(0).composite(screenshot, 87 * zoom, 123 * zoom)
+
+  overlayCopy.mask(mask);
+  overlay.composite(overlayCopy, 0, 0).write(destinationFile);
 }
 
 
@@ -25,6 +41,8 @@ module.exports = {
     return appWindow(screenshot, destinationFile, `${__dirname}/assets/application-overlay.png`, 1);
   },
   monitor: async function (screenshot, destinationFile) {
-    throw new Error("Unsupported operation")
+    // 2461 1390
+    // 87 123
+    return monitor(screenshot, destinationFile, 1);
   }
 }
