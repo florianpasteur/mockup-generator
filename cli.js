669b9e9a27f88bef12b75a7d1b786b77d498dbb2
Florian PASTEUR <florian.c.p@pm.me>
2022-05-31 14:19:25 +0200

    Default outputfile arguement

--- cli.js
+++ cli.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node --trace-warnings
 
 const mockupGenerator = require('./index');
+const fs = require('fs');
+const path = require('path');
 
-const [_, __, command, inputFile, outputFile] = process.argv;
+function getArguments() {
+    const [_, __, command, inputFile, outputFileArg] = process.argv;
+    if (!fs.existsSync(inputFile)) {
+        throw new Error('Input file does not exist');
+    }
+    const outputFile = outputFileArg || `${path.basename(inputFile, path.extname(inputFile))}.${command}.${path.extname(inputFile)}`
+    if (fs.existsSync(outputFile)) {
+        throw new Error('Output file already exists');
+    }
+    return [command, inputFile, outputFile];
+}
+
+const [command, inputFile, outputFile] = getArguments();
 
 switch (command) {
     case "browser":
@@ -15,6 +29,6 @@ switch (command) {
         return mockupGenerator.monitor(inputFile, outputFile);
 
     default:
-        console.error("Usage: browser|app|monitor inputFile outputFile")
+        console.error("Usage: browser|app|monitor inputFile [outputFile]")
         console.error(process.argv)
 }
